6725bfd4d65f3aeaa7fb390f6861db01209f48fe
Václav Pavlín <vaclav.pavlin@gmail.com>
2015-09-16 15:57:54 +0200

    Add prep.sh script

---
+++ prep.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+PROJECT=$1
+TO=$2
+
+if [ -e ".git/config" ] && grep -q "python-project" .git/config; then
+    echo "In correct git, proceeding..."
+else
+    echo "Not in correct git repo? Exiting..."
+    exit 1
+fi
+
+if ! [ -d ${TO} ]; then
+    echo "Creating destination directory: ${TO}"
+    mkdir -p ${TO}
+fi
+
+echo "Copying files to the destination directory"
+rsync -ar --verbose --exclude .git --exclude .*swp * ${TO} 
+
+echo "Removing prep script"
+rm -f ${TO}/prep.sh
+
+echo "Replacing \$PROJECT\$ with ${PROJECT}"
+files=$(grep -rl '\$PROJECT\$' . | sed "s#^.#/${TO}#")
+sed -i "s/\\\$PROJECT\\\$/${PROJECT}/g" ${files}
+
+echo "Changing project dir name to ${PROJECT}"
+mv ${TO}/project ${TO}/${PROJECT}
